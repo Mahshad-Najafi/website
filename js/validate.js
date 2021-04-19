60d5d4a15060cc46f2c356051ea2323b90670375
Mahshad Najafi <mahshadnajafi.r@gmail.com>
2021-04-18 20:59:24 -0400

    final relseae

--- js/validate.js
+++ js/validate.js
@@ -5,7 +5,19 @@ function validate(event) {
   // invalid, show the appropriate error message in the form, and stop the 
   // form from being submitted. If both values are valid, allow the form to be
   // submitted.
-  console.log('TODO - validate the longitude, latitude values before submitting');
+  // console.log('TODO - validate the longitude, latitude values before submitting');
+  var latitude = document.getElementById("lat").value;
+  var longitude = document.getElementById("long").value;
+  res = true;
+  if (!(latitude <= 90 && latitude >= -90)){
+    document.getElementById("laterrmsg").innerText = "* must be a valid Latitute (-90 to 90)";
+    res = false;
+  }
+  if (!(longitude <= 180 && longitude >= -180)){
+    document.getElementById("longerrmsg").innerText = "* must be a valid Longitude (-180 to 180)";
+    res = false;
+  }
+  return res;
 }
 
 // Wait for the window to load, then set up the submit event handler for the form.
